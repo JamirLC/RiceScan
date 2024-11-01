9875917ffa7d400131b10446c39fe7ff08ff4e7b
MaanArellano04 <princessarellano200404@gmail.com>
2024-11-01 19:10:46 +0800

    maan 1st

---
+++ training/modelTraining.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+num = 5
+
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+base_model.trainable = False #para hindi na magtrain
+
+x = base_model.ouput
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+output_layer = Dense(num,  activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=output_layer)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    r'C:\Users\Owner\Desktop\dataset\Rice_Image_Dataset', #local
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    r'C:\Users\Owner\Desktop\dataset\Rice_Image_Dataset', #local
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+history = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=validation_generator
+)
+
+#check loss at accuracy
+loss, accuracy = model.evaluate(validation_generator)
+print(f"Validation Loss: {loss}")
+print(f"Validation Accuracy: {accuracy}")
