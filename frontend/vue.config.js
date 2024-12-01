8f87f263b6c3403a618e8dc6b5eeafe776e01012
stu-Jamir <corderojamirlander@gmail.com>
2024-12-01 20:47:35 +0800

    https

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,4 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+module.exports = {
+  devServer: {
+    https: {
+      key: './key.pem',
+      cert: './cert.pem',
+    },
+    port: 8080, // Optional: Change the port if needed
+  },
+};
 
